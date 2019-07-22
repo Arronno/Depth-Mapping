1af3e901ebd69b7ac63acb212b741b2da8b4a744
Arronno <arronno111@gmail.com>
2019-07-22 18:29:33 +0900

    First Commit

---
+++ Assets/Scripts/ArmControl.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class ArmControl : MonoBehaviour
+{
+    public Transform LeftArm;
+    public Transform RightArm;
+
+    float yInitPos;
+
+    CollisionLog cL;
+
+    private void Start()
+    {
+        cL = FindObjectOfType<CollisionLog>();
+        yInitPos = LeftArm.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (cL.contactDistance > 0)
+            {
+                LeftArm.Translate(transform.forward * Time.deltaTime);
+                RightArm.Translate(-transform.forward * Time.deltaTime);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            LeftArm.Translate(Vector3.back * Time.deltaTime);
+            RightArm.Translate(Vector3.forward * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            LeftArm.Translate(Vector3.up * Time.deltaTime);
+            RightArm.Translate(Vector3.up * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (LeftArm.position.y > yInitPos & RightArm.position.y > yInitPos)
+            {
+                LeftArm.Translate(Vector3.down * Time.deltaTime);
+                RightArm.Translate(Vector3.down * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CollisionLog.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class CollisionLog : MonoBehaviour
+{
+    ArmControl ac;
+
+    float yDiff;
+    public float contactDistance;
+    float yInitPos;
+
+    private void Start()
+    {
+        ac = FindObjectOfType<ArmControl>();
+        contactDistance = 5.0f;
+        yInitPos = transform.position.y;
+    }
+
+    private void PickUpResponse(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Arm"))
+        {
+            if (transform.position.y >= yInitPos)
+            {
+                transform.position = new Vector3(0, ac.LeftArm.position.y - yDiff, 0);
+            }
+
+            transform.rotation = Quaternion.identity;
+        }
+    }
+
+
+    /*
+    private void OnCollisionEnter(Collision collision)
+    {
+        yDiff = Mathf.Abs(ac.LeftArm.position.y - transform.position.y);
+        //PickUpResponse(collision);     
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        PickUpResponse(collision);
+
+        if (collision.gameObject.CompareTag("Arm"))
+        {
+            //print("Collision Count: " + collision.contactCount);
+            //print("FFS: " + collision.collider);
+            //print("Position: " + collision.transform.position);
+            //print("Contacts: " + collision.contacts);
+
+            foreach (ContactPoint contact in collision.contacts)
+            {
+                Debug.DrawLine(contact.point, contact.point + contact.normal, Color.red, 0, true);
+                Debug.Log("Contact Point: " + contact.point);
+                Debug.Log("End Point: " + contact.normal);
+                contactDistance = contact.separation;
+                //Debug.Log("Separation: " + contactDistance);
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Arm"))
+        {
+            contactDistance = 5.0f;
+        }
+    }
+    */
+}
